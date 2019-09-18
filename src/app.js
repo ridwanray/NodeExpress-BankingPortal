59913d36fd23b4e47a251d0180339217b27237bd
rohan1311 <rohan.singh@sap.com>
2019-09-18 14:19:39 +0530

    first module

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
-    res.render('index.ejs', {title: 'Index'})
+    res.render('index', {title: 'Index'})
 })
 
 app.listen(3000, () => {

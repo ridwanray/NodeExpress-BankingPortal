0472ec10fa11eb40823c5a9ec26eab04276478a4
rohan1311 <rohan.singh@sap.com>
2019-09-25 14:31:49 +0530

    fifth commit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const fs = require('fs')
 const path = require('path')
 const express = require('express')
+const accountRoutes = require('./routes/accounts.js')
+const servicesRoutes = require('./routes/services.js')
 const app = express()
 const {accounts, users, writeJSON} = require('./data')
  
@@ -9,49 +11,15 @@ app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended: true}))
 
+app.use('/account', accountRoutes)
+app.use('/services', servicesRoutes)
+
 app.get('/', (req, res) => {
     res.render('index', {title: 'Account Summary', accounts: accounts})
 })
 
-app.get('/savings', (req, res) => {
-    res.render('account', {account: accounts.savings})
-})
-
-app.get('/checking', (req, res) => {
-    res.render('account', {account: accounts.checking})
-})
-
-app.get('/credit', (req, res) => {
-    res.render('account', {account: accounts.credit})
-})
-
 app.get('/profile', (req, res) => {
     res.render('profile', {user: users[0]})
-})
-
-app.get('/transfer', (req, res) => {
-    res.render('transfer')
-})
-
-app.post('/transfer', (req, res) => {
-    accounts[req.body.from].balance -= req.body.amount
-    accounts[req.body.to].balance = parseInt((accounts[req.body.to].balance))+parseInt(req.body.amount)
-    //console.log(parseInt((accounts[req.body.to].balance))+parseInt(req.body.amount))
-    //console.log(accounts[req.body.from])
-    //console.log(accounts[req.body.to])
-    writeJSON()
-    res.render('transfer', {message : "Transfer Completed"})
-})
-
-app.get('/payment', (req, res) => {
-    res.render('payment', {account: accounts.credit})
-})
-
-app.post('/payment', (req, res) => {
-    accounts.credit.balance -= req.body.amount
-    accounts.credit.available = parseInt(accounts.credit.available) + parseInt(req.body.amount)
-    writeJSON()
-    res.render('payment', { message: "Payment Successful", account: accounts.credit })
 })
 
 app.listen(3000, () => {
